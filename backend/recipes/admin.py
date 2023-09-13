5aa70a6f54db78f9b5b9e5ef583aed0a13c4b35c
Алишер Ризабеков <magneto06@mail.ru>
2023-09-13 13:29:42 +0600

    2

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Ingredient, Tag
+from .models import Ingredient, Tag, Recipe
 admin.site.register(Ingredient)
 admin.site.register(Tag)
+admin.site.register(Recipe)
 # Register your models here.
